85cbd86cc676a325892a149a3e6646ecf479a3c3
naira0001 <n93217894@gmail.com>
2024-11-04 20:25:49 +0600

    lesson2

---
+++ lessons/lesson1.py
@@ -0,0 +1,37 @@
+# OOP - Объектно-ориентированное программирование
+
+# types_python = 1, 'str', 1.2, True, {}, [], ()
+#
+# print(type('str'))
+
+# def a(b):
+#     print(b)
+#
+# a(123)
+
+# CamelCase - snike_case
+
+class Car:
+
+
+    def drive(self):
+        print('Машина едет', self.model)
+
+    def __init__(self, model, volume, age):
+        self.model = model
+        self.volume = volume
+        self.age = age
+
+    def start(self):
+        print("Машина завелась в", self.age)
+
+
+
+# Экземпляр
+audi = Car(model="A8", age="2000", volume="2.8")
+mazda = Car(model="RX-8", volume="3.0", age="2001")
+
+audi.drive()
+mazda.drive()
+
+print(mazda)
