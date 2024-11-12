888197cd0912f5bb86418364500424b1d24d34fb
naira0001 <n93217894@gmail.com>
2024-11-12 13:07:31 +0600

    lesson4

---
+++ lessons/lesson4.py
@@ -0,0 +1,30 @@
+# Тема урока Пакеты, модули, библиотеки и Фреймворки
+# Урок 4. Пакеты, модули
+from symtable import Class
+
+# Встроенный модуль
+# import random
+#
+# print(random(1, 10))
+#
+# # кастомные локальные
+# from lesson1 import Person
+
+# внешние модули
+from colorama import  Fore, Back, Style
+
+# init()
+
+print(Fore.RED + "Some thing text")
+print(Back.GREEN)
+print("Новый текс")
+print('test')
+print(Style.RESET_ALL)
+print('new tekst')
+
+
+# from django.db.models import Model
+#
+# class Test:
+#     pole = Model.check()
+
